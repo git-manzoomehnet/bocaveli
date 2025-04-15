17fa5a94257a4cfff40ee01ab548c6c2b5a7fc06
Zeinab ansarian <zeinabansarian6280@gmail.com>
2025-04-15 17:12:14 +0330

    add loader

---
+++ web/assets/js/loader.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (params) {
+    console.log(document.querySelector(".loaderMain"));
+    
+    document.querySelector(".loaderMain").style.scale = "0"
+})
